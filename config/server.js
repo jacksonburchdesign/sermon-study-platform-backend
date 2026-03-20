9b5b6be680a86724ed6e64b3acd2283a88b3ae6c
jacksonburchdesign <jacksonburch95@gmail.com>
2026-03-20 02:18:54 -0500

    Fix: Enabled proxy trust and absolute URL for OAuth

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // 1. Tell Strapi its live web address (Fixes the yellow warning)
+  url: env('PUBLIC_URL', ''), 
+  // 2. Tell Strapi to trust Render's HTTPS front door (Fixes the red crash error)
+  proxy: true, 
   app: {
     keys: env.array('APP_KEYS'),
   },
